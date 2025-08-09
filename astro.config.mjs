3a355e7f1ebce0e5de4bdb881e546370e232a51f
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2025-08-10 00:03:57 +0530

    finally works i hope

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
